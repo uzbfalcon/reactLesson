052fb95f6f4d675b9a4f0fd4f01a583686196bcf
uzbfalcon <lochinbekkhujamov1217@gmail.com>
2022-11-10 12:37:03 +0500

    table

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,15 @@ class Student extends React.Component {
       data: student,
       name: 'Lochinbek',
       surname: 'Khujamov',
+      active: null,
     };
   }
   render() {
-    
+
     const onFilter = (e) => {
-      console.log(e.target.value);
-      let res = student.filter((value) => value.name.includes(e.target.value))
+      // console.log(e.target.value);
+      const { value } = e.target
+      let res = student.filter((item) => item.name.toLowerCase().includes(value.toLocaleLowerCase()))
       this.setState({ data: res })
 
     }
@@ -26,7 +28,18 @@ class Student extends React.Component {
       let res = this.state.data.filter((value) => value.id !== id);
       this.setState({ data: res })
     }
-
+    const onEdit = ({ id, name, status, address, univ, job }, isSave) => {
+      if (isSave) {
+        this.setState({ active: null })
+      }
+      else {
+        this.setState({
+          name : name,
+          status:status,
+          active: { id, name, status, address, univ, job }
+        })
+      }
+    }
 
     return (
       <div>
@@ -36,33 +49,49 @@ class Student extends React.Component {
           <table border={0} width={'90%'}>
             <thead>
               <tr>
-                <th>ID</th>
+                <th className="sticky-col first-col">ID</th>
                 <th>Name</th>
+                <th>Address</th>
                 <th>Status</th>
-                <th>Edit</th>
+                <th>Univ</th>
+                <th>Job</th>
                 <th>Delete</th>
+                <th className="sticky-col second-col">Edit</th>
               </tr>
             </thead>
             <tbody>
-              {this.state.data.map(({ id, name, status }) => {
+              {this.state.data.length ? (
+                this.state.data.map(({ id, name, status, address, univ, job }) => {
                 return (
                   <tr key={id}>
-                    <td>{id}</td>
-                    <td>{name}</td>
-                    <td>{status}</td>
+                    <th className="first-col sticky-col">{id}</th>
+                    <td>{
+                      this.state.active?.id === id ? <input type="text" value={this.state.active.name}/> : name
+                    }</td>
+                    <td>{address}</td>
+                    <td>{
+                      this.state.active?.id === id ? <input type="text" value={this.state.active.status} /> : status
+                    }</td>
+                    <td>{
+                      this.state.active?.id === id ? <input type="text" value={this.state.active.univ}/> : univ
+                    }</td>
+                    <td>{
+                      this.state.active?.id === id ? <input type="text" value={this.state.active.job}/> : job
+                    }</td>
 
-                    <td>
-                      <button className="edit">edit</button>
-                    </td>
                     <td>
                       <button className="delete" onClick={() => onDelete(id)}>delete</button>
                     </td>
+                    <td className="sticky-col second-col">
+                      <button className="edit" onClick={() => onEdit({ id, name, status, address, univ, job }, this.state.active?.id === id)}>{this.state.active?.id === id ? 'save' : 'edit'}</button>
+                    </td>
                   </tr>
                 )
-              })}
+              })) : (
               <td colSpan={5}>
                 <h1>Noup</h1>
-              </td>
+              </td>)
+              }
             </tbody>
           </table>
         </div>
